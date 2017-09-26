52a178ea0da52feb6efa1b8245bcc3e472dd4113
puppalasatish <satishpuppala87@gmail.com>
2017-09-26 12:36:46 +0530

    added satish

--- sample.cpp
+++ sample.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int  main(){
 	cout << "hello dude" << endl ;
 	cout << "hello " << endl ;
-	cout << "hello aneesh" << endl ;
+	cout << "hello aneesh satish" << endl ;
 	return 0;
 }
