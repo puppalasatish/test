68db14d023a8aaf6d01a24151491c5e9000cbd3f
satish puppala <satishpuppala87@gmail.com>
2021-05-12 16:56:04 +0530

    Update sample.cpp
    update to latest

--- sample.cpp
+++ sample.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int  main(){
 	cout << "hello dude" << endl ;
 	cout << "hello " << endl ;
-	cout << "hello aneesh kalyan satish" << endl ;
+	cout << "hello teja" << endl ;
 	return 0;
 }
