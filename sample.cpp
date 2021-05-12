342933f86546b9416cae6618a542132cdacf8554
spuppalaa-rythmos <spuppala@rythmos.com>
2021-05-12 17:05:26 +0530

    Update sample.cpp
    added my name

--- sample.cpp
+++ sample.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int  main(){
 	cout << "hello dude" << endl ;
 	cout << "hello " << endl ;
-	cout << "hello teja" << endl ;
+	cout << "hello teja satih" << endl ;
 	return 0;
 }
