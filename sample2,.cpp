a784b96eb47d2f1692fef6eb23676059b04945c1
satish puppala <satishpuppala87@gmail.com>
2021-10-20 16:07:28 +0530

    Update sample2,.cpp

--- sample2,.cpp
+++ sample2,.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int  main() {
 	cout << "hello dude how are you" << endl;
 	cout << "hello " << endl;
-	cout << "hello teja how are " << endl;
+	cout << "hello teja how are test this one " << endl;
 	return 0;
 }
