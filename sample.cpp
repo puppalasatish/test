cb1894389455d197a6e38c5e15392716850bb874
alekhyam44 <107926081+alekhyam44@users.noreply.github.com>
2022-06-21 18:58:44 +0530

    name change
    name

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int  main(){
 	cout << "hello dude how are you				" << endl ;
-	cout << "hello 		100		" << endl ;
-	cout << "hello alli	99				" << endl ;
+	cout << "hello 		" << endl ;
+	cout << "hello abc			" << endl ;
 	return 0;
 }
