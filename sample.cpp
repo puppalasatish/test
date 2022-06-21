8639dac7ff5800d9ce9a585714170069d41f314d
puppalasatish <satishpuppala87@gmail.com>
2022-06-21 16:54:08 +0530

    100

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int  main(){
 	cout << "hello dude how are you				" << endl ;
-	cout << "hello 				" << endl ;
+	cout << "hello 		100		" << endl ;
 	cout << "hello alli	1				" << endl ;
 	return 0;
 }
